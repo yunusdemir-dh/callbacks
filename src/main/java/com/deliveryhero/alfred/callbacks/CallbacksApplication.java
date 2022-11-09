cbe5a8912d2a7a1bbb58688ec10d75b48326e363
Yunus Demir <yunus.demir@deliveryhero.com>
2022-11-09 10:19:28 +0100

    PAYORC-0: project created

---
+++ src/main/java/com/deliveryhero/alfred/callbacks/CallbacksApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryhero.alfred.callbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallbacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deliveryhero/alfred/callbacks/CallbacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryhero.alfred.callbacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallbacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
